badf06e3e94db28aa37090757f6746ea728ebb54
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-01 21:38:09 -0400

    Fix: update return diff between items by merchant and average

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -33,20 +33,30 @@ RSpec.describe SalesAnalyst do
     expect(sales_analyst.average_items_per_merchant).to eq(2.88)
   end
 
-  it 'returns difference between given number and the mean' do
+  it 'returns difference between number of items for specified merchant and average items' do
     sales_engine = SalesEngine.from_csv({
       :items => "./data/items.csv",
       :merchants => "./data/merchants.csv"
       })
     sales_analyst = sales_engine.analyst
 
-    expect(sales_analyst.difference_between_number_and_mean(5)).to eq(2.12)
+    expect(sales_analyst.difference_between_merchant_items_and_mean(12334135)).to eq(1.88)
 
-    expect(sales_analyst.difference_between_number_and_mean(20)).to eq(17.12)
+    expect(sales_analyst.difference_between_merchant_items_and_mean(12334315)).to eq(2.12)
 
   end
 
-  it "can determine standard deviation" do
+  xit 'can return sum of differences squared' do
+    sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+      })
+    sales_analyst = sales_engine.analyst
+
+    expect(sales_analyst.sum_of_differences_squared).to eq(5)
+  end
+
+  xit "can determine standard deviation" do
     expect(@sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
   end
 end
