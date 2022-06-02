f8ae6bfaeec0c96e20d6583c2cfb44ec5a9464c4
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-01 21:22:52 -0400

    Test: returns diff between number and mean

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -33,7 +33,20 @@ RSpec.describe SalesAnalyst do
     expect(sales_analyst.average_items_per_merchant).to eq(2.88)
   end
 
-  xit "can determine standard deviation" do
+  it 'returns difference between given number and the mean' do
+    sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+      })
+    sales_analyst = sales_engine.analyst
+
+    expect(sales_analyst.difference_between_number_and_mean(5)).to eq(2.12)
+
+    expect(sales_analyst.difference_between_number_and_mean(20)).to eq(17.12)
+
+  end
+
+  it "can determine standard deviation" do
     expect(@sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
   end
 end
