c51cea839da6586f7be8fcb41456cf785840444a
Brenna Stuart <brennacodes@gmail.com>
2022-06-08 16:09:37 -0600

    Feat: refactor in invoice

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -14,9 +14,6 @@ class Invoice
     @id = input[:id].to_i
     @customer_id = input[:customer_id].to_i
     @merchant_id = input[:merchant_id].to_i
-    # status needs to be changed to a symbol in the
-    # analyst class 'invoice_status' method, not here
-    # or it breaks tests
     @status = input[:status]
     @created_at = input[:created_at]
     @updated_at = input[:updated_at]
